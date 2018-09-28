418c82d29aec17090ecb1a21997cabd5de0801f5
Lavínia Matoso <laviniamatosof@alu.ufc.br>
2018-09-28 13:41:43 -0300

    Criando modelo Gig e Genre

---
+++ GigHub/Models/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace GigHub.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public DbSet<Gig> Gigs { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
---
+++ GigHub/Models/Gig.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace GigHub.Models
+{
+    public class Gig
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public ApplicationUser Artista { get; set; }
+
+        public DateTime Data { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Local { get; set; }
+
+        [Required]
+        public Genre genero { get; set; }
+    }
+
+
+}
