fac4613d37badae13e7d95a870a849eced7ef2e3
Lavínia Matoso <laviniamatosof@alu.ufc.br>
2018-10-09 08:26:57 -0300

    Adicionando view Create

---
+++ GigHub/ViewModels/GigModelViewForm.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using GigHub.Models;
+
+namespace GigHub.ViewModels
+{
+    public class GigModelViewForm
+    {
+        public string Local { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public int Genre { get; set; }
+        public IEnumerable<Genre> Genres { get; set; }
+    }
+}
